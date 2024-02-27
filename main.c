4c366db1be7e896a29c89f9c62f5fd5e4e87ef9a
LowellTN <lowel.buzzi@telecomnancy.eu>
2024-02-27 20:11:28 +0100

    Le gitignore marche ouuuuuuuu

--- main.c
+++ main.c
@@ -2,12 +2,63 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
+void SDL_ExitWithError(const char *message);
+
 int main(int argc, const char * argv[]) {
     SDL_version nb;
     SDL_VERSION(&nb);
 
     printf("Bienvenue sur la SDL %d,%d,%d!\n", nb.major, nb.minor,nb.patch);
 
-    SDL_Init( SDL_INIT_VIDEO );
-    return 0;
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
+    if(SDL_Init( SDL_INIT_VIDEO ) != 0){
+        SDL_Log("Erreur : Initialisation SDL > %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    window = SDL_CreateWindow("Scarf",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,0);
+
+
+    if (window == NULL) {
+        SDL_Log("Erreur : Initialisation SDL > %s\n", SDL_GetError());
+        exit(EXIT_FAILURE); 
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if(SDL_SetRenderDrawColor(renderer,112,168,237, SDL_ALPHA_OPAQUE) != 0) {
+        SDL_ExitWithError("Impossible de changer la couleur pour le rendu");
+    }
+
+    if(SDL_RenderDrawPoint(renderer, 100, 450) != 0){
+        SDL_ExitWithError("Impossible de dessiner un point");
+    }
+
+    if(SDL_RenderDrawLine(renderer, 50,50,500,500) != 0){
+        SDL_ExitWithError("Impossible de dessiner un point");
+    }
+
+
+    if(renderer == NULL) {
+        SDL_ExitWithError("Creation de rendu échouée");
+
+    }
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(10000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+
+void SDL_ExitWithError(const char *message){
+    SDL_Log("ERREUR : %s > %s\n", message, SDL_GetError());
+    SDL_Quit;
+    exit(EXIT_FAILURE);
 }
