5675be7abbd6b4728693b4b55b194c8c4817e551
LowellTN <lowel.buzzi@telecomnancy.eu>
2024-04-02 18:12:45 +0200

    Suppression des fichiers player que nathan devra faire - les segmentation fault sur main viennent uniquement du fsanitize le code marche parfaitement sinon

--- map.h
+++ map.h
@@ -4,7 +4,7 @@
 SDL_Texture *bgTextures[6];
 
 Map *init_map(char*) ;
-void loadBackgroundTextures(SDL_Renderer*,SDL_Texture*);
+void loadBackgroundTextures(SDL_Renderer*,SDL_Texture*[]);
 void display_tile(SDL_Renderer*, int, int, int, int , char*);
 int draw_map(SDL_Renderer*, Map*, char*) ;
 
