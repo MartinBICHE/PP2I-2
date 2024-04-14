d08f935053734c65a38ab0485289d819964040e7
lilian <li4bou@gmail.com>
2024-04-14 14:16:48 +0200

    parallax sur le background pour les mouvements de camera et optimisation sur les textures en dehors de la fenetre

--- map.h
+++ map.h
@@ -4,7 +4,7 @@
 Map *initMap(char*);
 float updateCam(float, float);
 void loadBackgroundTextures(SDL_Renderer*,SDL_Texture*[],int);
-int drawBackground(SDL_Renderer*, SDL_Texture*[], int);
+int drawBackground(SDL_Renderer*, SDL_Texture*[], int, float);
 void display_tile(SDL_Renderer*, int, int, int, int , char*, float);
 int drawMap(SDL_Renderer*, Map*, char*, float);
 
